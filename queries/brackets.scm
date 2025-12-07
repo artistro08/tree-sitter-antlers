e46af2a611d6a1421d50dee8866a4e284546f256
Devin Green <design@devingreen.me>
2025-12-07 15:51:14 -0600

    commits

---
+++ queries/brackets.scm
@@ -0,0 +1,17 @@
+; Bracket matching for Antlers
+
+; Parentheses
+("(" @open ")" @close)
+
+; Square brackets
+("[" @open "]" @close)
+
+; Curly braces (arrays)
+("{" @open "}" @close)
+
+; Antlers delimiters
+("{{" @open "}}" @close)
+
+; Strings
+("\"" @open "\"" @close)
+("'" @open "'" @close)
